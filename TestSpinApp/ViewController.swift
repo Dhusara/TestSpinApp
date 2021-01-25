e582ee8eb7129778646328c1f3d475dfdf8dab32
Sergei <dhusara@icloud.com>
2021-01-25 16:14:11 +0200

    test

--- TestSpinApp/ViewController.swift
+++ TestSpinApp/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let image1 = UIImage(named: "apple")
+    let image2 = UIImage(named: "cherry")
+    let image3 = UIImage(named: "star")
+    
+    func randomImage(image: UIImageView?) -> UIImage? {
+        let images = [image1, image2, image3]
+        let randomImage = images.randomElement()
+        
+        return randomImage as? UIImage
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBOutlet weak var slot1: UIImageView!
+    @IBOutlet weak var slot2: UIImageView!
+    @IBOutlet weak var slot3: UIImageView!
+    var image: UIImage?
+    
+    @IBAction func spinButton(_ sender: Any) {
+        self.slot1.image = changeImage1()
+        self.slot2.image = changeImage2()
+        self.slot3.image = changeImage3()
+    }
+    
+    func changeImage1() -> UIImage? {
+        let image = randomImage(image: slot1)
+        return image
+    }
+    
+    func changeImage2() -> UIImage? {
+        let image = randomImage(image: slot2)
+        return image
+    }
+    
+    func changeImage3() -> UIImage? {
+        let image = randomImage(image: slot3)
+        return image
+    }
 
 }
 
